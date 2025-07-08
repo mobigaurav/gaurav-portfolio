af59200503c231b3b869b0e0441accf2d43a9863
Gaurav Kumar <meetgaurav1@gmail.com>
2025-07-08 15:25:39 -0400

    updated change

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -2,6 +2,13 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import { MDXRemote } from "next-mdx-remote/rsc";
+import React, { JSX } from "react";
+
+interface BlogParams {
+  params: {
+    slug: string;
+  };
+}
 
 export async function generateStaticParams() {
   const files = fs.readdirSync(path.join(process.cwd(), "content/blog"));
@@ -10,7 +17,7 @@ export async function generateStaticParams() {
   }));
 }
 
-export default function BlogPost({ params }: { params: { slug: string } }) {
+export default function BlogPost({ params }: BlogParams): JSX.Element {
   const filePath = path.join(process.cwd(), "content/blog", `${params.slug}.mdx`);
   const source = fs.readFileSync(filePath, "utf8");
   const { content } = matter(source);
