4b5a2f3a6fdc5fbdb0ca8aa46fa1f5ae53ade3fd
Gaurav Kumar <meetgaurav1@gmail.com>
2025-07-08 15:44:37 -0400

    chore: disable type check and linting for Vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 // next.config.ts ✅
 import withMDX from '@next/mdx';
+/** @type {import('next').NextConfig} */
 
 const withMDXPlugin = withMDX({
   extension: /\.mdx?$/,
@@ -7,6 +8,12 @@ const withMDXPlugin = withMDX({
 
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'mdx'],
+   typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   // other settings here
 };
 
